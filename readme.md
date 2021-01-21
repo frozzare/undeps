f21317f368ce95e90abb5b2d12d8b3340777519d
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2021-01-21 17:14:41 +0100

    First commit

---
+++ readme.md
@@ -0,0 +1,19 @@
+# undeps
+
+> work in progress
+
+## config
+
+```js
+module.exports = {
+  pattern: './src/**/*.+(js|ts|jsx|tsx|vue)',
+  exclude: [],
+  excludeFn: (dep) => true,
+  files: [],
+  types: false,
+};
+```
+
+## License
+
+MIT © [Fredrik Forsmo](https://github.com/frozzare)
---
+++ src/index.js
@@ -0,0 +1,87 @@
+const R = require('ramda');
+const glob = require('glob');
+const fs = require('fs');
+
+const cwd = process.cwd();
+const pkg = require(`${cwd}/package.json`);
+
+const config = {
+  ...{
+    pattern: './src/**/*.+(js|ts|jsx|tsx|vue)',
+    exclude: [],
+    excludeFn: (dep) => true,
+    files: [],
+    types: false,
+  },
+  ...(fs.existsSync(`${cwd}/undeps.config.js`)
+    ? require(`${cwd}/undeps.config.js`)
+    : {}),
+};
+
+const binaries = (p) => {
+  try {
+    const { bin } = require(`${cwd}/node_modules/${p}/package.json`);
+    return typeof bin === 'string' ? { [p]: [bin] } : bin;
+  } catch (err) {
+    return;
+  }
+};
+
+const hasBinaries = (p) => {
+  const b = binaries(p);
+  return b ? !!Object.keys(b).length : false;
+};
+
+const deps = Object.keys(pkg.dependencies)
+  .concat(Object.keys(pkg.devDependencies))
+  .filter(config.excludeFn)
+  .filter((p) => (config.types ? true : p.indexOf('@types/') === -1))
+  .filter((p) => !config.exclude.includes(p))
+  .filter((p) => !hasBinaries(p));
+
+const files = {
+  './package.json': JSON.stringify({
+    ...pkg,
+    scripts: Object.values(pkg.scripts),
+    dependencies: {},
+    devDependencies: {},
+    resolutions: {},
+  }),
+  ...config.files.reduce(
+    (prev, file) => ({
+      ...prev,
+      [file]: fs.readFileSync(file).toString(),
+    }),
+    {}
+  ),
+  ...glob.sync(config.pattern).reduce(
+    (prev, file) => ({
+      ...prev,
+      [file]: fs.readFileSync(file).toString(),
+    }),
+    {}
+  ),
+};
+
+const fileNames = Object.keys(files);
+
+const checkFiles = (dep) =>
+  !!fileNames.find((fileName) => files[fileName].indexOf(dep) !== -1);
+
+const diff = R.pipe(
+  R.reduce(
+    (prev, dep) => ({
+      ...prev,
+      [dep]: checkFiles(dep),
+    }),
+    {}
+  ),
+  R.filter((p) => !p)
+)(deps);
+
+console.log(
+  `Found ${Object.keys(deps).length} dependenc${
+    Object.keys(deps).length > 1 ? 'ies' : 'y'
+  } and ${Object.keys(diff).length} are unused:`
+);
+console.log(`\n${Object.keys(diff).join('\n')}`);
