a31f5af3470b96336ff29aacd4ae57daa9018f9d
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2021-06-18 15:13:49 +0200

    Fix issue with node_modules in packages folder

--- src/config.js
+++ src/config.js
@@ -5,7 +5,7 @@ const defaultConfig = (cwd) => ({
   excludeFn: (dep) => true,
   files: [],
   pattern: `${cwd}/src/**/*.+(js|ts|jsx|tsx|vue)`,
-  packages: `${cwd}/{package.json,packages/**/package.json}`,
+  packages: `${cwd}/{package.json,packages/*/package.json}`,
   types: false,
 });
 
